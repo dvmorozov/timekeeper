cb5a72127295175ca5f8220c2f749c442e8a90e1
Morozov <Morozov@d504a619-eb68-f843-b33a-476b659037dc>
2014-04-06 15:44:20 +0000

    Начал работать над дизайном главной страницы.
    https://www.evernote.com/shard/s132/nl/14501366/a5aa12d0-8033-471f-be9d-4675825e5622
    
    git-svn-id: https://rationalcity.com:8443/svn/Morozov/my/timekeeper/win%20phone%208@2677 d504a619-eb68-f843-b33a-476b659037dc

---
+++ TimeKeeper/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TimeKeeper.Resources;
+
+namespace TimeKeeper
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ TimeKeeper/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TimeKeeper.Resources;
+using System.Collections.ObjectModel;
+
+namespace TimeKeeper
+{
+    //  This class represents the expense category.
+    public class Category
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Category(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        ObservableCollection<Category> categories = new ObservableCollection<Category>();
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CategoryList.ItemsSource = categories;
+
+            categories.Add(new Category("Germany"));
+            categories.Add(new Category("France"));
+            categories.Add(new Category("Italy"));
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var i = 0;
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
