76d5dbdafeab5270e780eb34854c80de62b46ac5
Morozov <Morozov@d504a619-eb68-f843-b33a-476b659037dc>
2014-04-30 16:53:11 +0000

    Работает добавление категории.
    https://www.evernote.com/shard/s132/nl/14501366/a5aa12d0-8033-471f-be9d-4675825e5622
    
    git-svn-id: https://rationalcity.com:8443/svn/Morozov/my/timekeeper/win%20phone%208@2762 d504a619-eb68-f843-b33a-476b659037dc

---
+++ TimeKeeper/AddCategoryPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace TimeKeeper
+{
+    public partial class AddCategory : PhoneApplicationPage
+    {
+        public AddCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+            MainPage.CategoryName = TextBoxCategoryName.Text;
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
--- TimeKeeper/MainPage.xaml.cs
+++ TimeKeeper/MainPage.xaml.cs
@@ -9,14 +9,20 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using TimeKeeper.Resources;
 using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace TimeKeeper
 {
-    //  This class represents the expense category.
+    //  This class represents the work category.
+    [DataContract]
     public class Category
     {
         private string _name;
 
+        [DataMember]
         public string Name
         {
             get { return _name; }
@@ -29,44 +35,122 @@ namespace TimeKeeper
         }
     }
 
+    [DataContract]
+    public class TimeExpensesData
+    {
+        [DataMember]
+        public ObservableCollection<Category> categories;
+
+        public TimeExpensesData()
+        {
+            //  Must be initialized in the constructor.
+            categories = new ObservableCollection<Category>();
+        }
+
+        public void AddCategory(string name)
+        {
+            categories.Add(new Category(name));
+        }
+    }
+
     public partial class MainPage : PhoneApplicationPage
     {
-        ObservableCollection<Category> categories = new ObservableCollection<Category>();
+        private TimeExpensesData _data;
+        private string _fileName = "TimeExpensesData";
+
+        public static string CategoryName;
+
+        private void LoadCategories()
+        {
+            try
+            {
+                //  Load data from file.
+                var fileStorage = IsolatedStorageFile.GetUserStoreForApplication();
+
+                if (fileStorage.FileExists(_fileName))
+                {
+                    using (var stream = new IsolatedStorageFileStream(_fileName, FileMode.Open, fileStorage))
+                    {
+                        //  Deserialize the object.
+                        var ser = new DataContractJsonSerializer(typeof(TimeExpensesData));
+                        _data = (TimeExpensesData)ser.ReadObject(stream);
+                    }
+                }
+                
+                if(_data == null)
+                    _data = new TimeExpensesData();
+
+                //???
+                //for (var i = 0; i < 50; i++)
+                //    _data.categories.Add(new Category((i * 100000).ToString()));
+            }
+            catch
+            {
+                //  TODO: show message!
+            }
+        }
+
+        private void SaveCategories()
+        {
+            //  Save data to file.
+            try
+            {
+                var fileStorage = IsolatedStorageFile.GetUserStoreForApplication();
+                using (var stream = new IsolatedStorageFileStream(_fileName, FileMode.OpenOrCreate, fileStorage))
+                {
+                    var ser = new DataContractJsonSerializer(typeof(TimeExpensesData));
+                    ser.WriteObject(stream, _data);
+                }
+            }
+            catch(Exception e) 
+            { 
+                // TODO: show message! 
+                var i = 0;
+            }
+        }
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+            LoadCategories();
 
-            CategoryList.ItemsSource = categories;
+            CategoryList.ItemsSource = _data.categories;
 
-            categories.Add(new Category("Germany"));
-            categories.Add(new Category("France"));
-            categories.Add(new Category("Italy"));
+            BuildLocalizedApplicationBar();
+        }
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+        //  Replaces names with localized strings.
+        private void BuildLocalizedApplicationBar()
+        {
+            for(var i = 0; i < ApplicationBar.Buttons.Count; i++)
+            {
+                var btn = ApplicationBar.Buttons[i] as ApplicationBarIconButton;
+                if (btn != null && btn.Text.ToLower() == "add")
+                {
+                    btn.Text = AppResources.AppBarButtonAddText;
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var i = 0;
+            //???
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AddCategoryPage.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (CategoryName != null)
+            {
+                //  Return by pressing "Add category".
+                _data.AddCategory(CategoryName);
+                SaveCategories();
+            }
+        }
     }
 }
