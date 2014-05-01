bd6adf18d0a0b54ba8ceadf93fa4cc027a698617
Morozov <Morozov@d504a619-eb68-f843-b33a-476b659037dc>
2014-05-01 15:55:05 +0000

    Добавил файлы.
    https://www.evernote.com/shard/s132/nl/14501366/a5aa12d0-8033-471f-be9d-4675825e5622
    
    git-svn-id: https://rationalcity.com:8443/svn/Morozov/my/timekeeper/win%20phone%208@2765 d504a619-eb68-f843-b33a-476b659037dc

---
+++ TimeKeeper/DeleteCategoryPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.ComponentModel;
+using System.Windows.Data;
+using TimeKeeper.Resources;
+
+namespace TimeKeeper
+{
+    public partial class DeleteCategoryPage : PhoneApplicationPage
+    {
+        public DeleteCategoryPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            CategoryList.ItemsSource = MainPage.Data.categories;
+        }
+
+        private void DeleteSelectedItem()
+        {
+            var item = (Category)CategoryList.SelectedItem;
+            if (item != null)
+            {
+                var name = item.Name;
+                if (MessageBox.Show(string.Format(AppResources.DeleteCategoryMessage, name), AppResources.DeleteCategoryCaption, MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    MainPage.Data.DeleteCategory(item);
+                }
+            }
+        }
+
+        private void CategoryList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeleteSelectedItem();
+        }
+
+        //  This allows to repeat the deleting procedure if user cancels it for the first time.
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteSelectedItem();
+        }
+    }
+}
