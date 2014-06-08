d5ca1fc1c3f02329922bd5a2fa1d8d0c1c8eff81
Dmitry Morozov <dvmorozov@hotmail.com>
2014-06-08 12:00:41 +0600

    Boundaries of the silence interval are saved.
    https://www.evernote.com/shard/s132/nl/14501366/3fd5872d-8014-4a16-84cf-366189a59c12

--- TimeKeeper/ConfigPage.xaml.cs
+++ TimeKeeper/ConfigPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
 
 namespace TimeKeeper
 {
@@ -15,6 +16,7 @@ namespace TimeKeeper
         public ConfigPage()
         {
             InitializeComponent();
+            LoadSilenceInterval();
         }
 
         private void ApplicationBarIconActivitiesButton_Click(object sender, EventArgs e)
@@ -25,6 +27,65 @@ namespace TimeKeeper
         private void ApplicationBarIconButtonStatistics_Click(object sender, EventArgs e)
         {
             NavigationService.Navigate(new Uri("/StatisticsPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private const string silenceTimeFrom = "SilenceTimeFrom";
+        private const string silenceTimeTo = "SilenceTimeTo";
+
+        private void UpdateSilenceInterval()
+        {
+            if (_isConfigUpdateBlocked) return;
+
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+
+            var t = ((DateTime)TimePickerFrom.Value).ToShortTimeString();
+            if (!settings.Contains(silenceTimeFrom))
+                settings.Add(silenceTimeFrom, t);
+            else
+                settings[silenceTimeFrom] = t;
+
+            t = ((DateTime)TimePickerTo.Value).ToShortTimeString();
+            if (!settings.Contains(silenceTimeTo))
+                settings.Add(silenceTimeTo, t);
+            else
+                settings[silenceTimeTo] = t;
+
+            settings.Save();
+        }
+
+        private bool _isConfigUpdateBlocked = false;
+
+        private void LoadSilenceInterval()
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+
+            //  Prevents saving default values from event handlers 
+            //  during the initialization process.
+            _isConfigUpdateBlocked = true;
+
+            if (settings.Contains(silenceTimeFrom))
+            {
+                var t = settings[silenceTimeFrom] as string;
+                TimePickerFrom.Value = DateTime.Parse(t);
+            }
+
+            if (settings.Contains(silenceTimeTo))
+            {
+                var t = settings[silenceTimeTo] as string;
+                TimePickerTo.Value = DateTime.Parse(t);
+            }
+
+            _isConfigUpdateBlocked = false;
+        }
+
+        private void TimePickerFrom_ValueChanged(object sender, DateTimeValueChangedEventArgs e)
+        {
+            UpdateSilenceInterval();
+        }
+
+        private void TimePickerTo_ValueChanged(object sender, DateTimeValueChangedEventArgs e)
+        {
+            UpdateSilenceInterval();
         }
     }
 }
