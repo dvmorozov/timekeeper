168148948412715a76c13525b95934ffdcba97c1
Dmitry Morozov <dvmorozov@hotmail.com>
2016-10-07 23:10:41 +0500

    Test script for Todoist API was added.
    https://action.mindjet.com/task/14747736

---
+++ TestTodoistAPI/todoist_api.py
@@ -0,0 +1,40 @@
+import todoist
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Query Todoist API.')
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+def queryTodoistAPI(userName, password):
+    api = todoist.TodoistAPI()
+    user = api.user.login(userName, password)
+    #print(user['full_name'])
+
+    response = api.sync()
+    #for project in response['projects']:
+    #    print(project['name'])
+
+    for item in response['items']:
+        if item['checked'] == 0:
+            s = item['content']
+            name = ""
+            url = ""
+            if s.find('http') == -1:
+                name = s
+            else:
+                l = s.split()
+                url = l[0]
+                name = " ".join(l[1:])
+
+            if name[0] == '(':
+                name = name[1:]
+            if name[len(name) - 1] == ')':
+                name = name[:len(name) - 1]
+
+            print(name)
+            print(url)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    queryTodoistAPI(args.username, args.password)
