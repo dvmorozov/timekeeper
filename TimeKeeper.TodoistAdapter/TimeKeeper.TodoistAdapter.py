ac3615e7d98e9dedf2b4221c92675349353fd8d5
Dmitry Morozov <dvmorozov@hotmail.com>
2016-10-17 22:50:50 +0500

    Converting into JSON has been implemented.
    https://action.mindjet.com/task/14747735

--- TimeKeeper.TodoistAdapter/TimeKeeper.TodoistAdapter.py
+++ TimeKeeper.TodoistAdapter/TimeKeeper.TodoistAdapter.py
@@ -2,13 +2,35 @@
 import todoist
 import argparse
 import configparser
+import base64
 
 class Task_1:
-    def get_Id():
-        return 0
+    id = None
+    name = None
+    url = None
 
-    def get_Name():
-        return ""
+    def __init__(self, id, name, url, **kwargs):
+        self.id = id
+        self.name = name
+        self.url = url
+
+    def toJSON(self):
+        result = '{"id" : "' + str(base64.standard_b64encode(str(self.id).encode()))
+        result += '", "name" : "' + str(base64.standard_b64encode(self.name.encode()))
+        result += '", "url" : "' + str(base64.standard_b64encode(self.url.encode())) + '"}'
+        return result
+
+class TaskList:
+    list = []
+
+    def toJSON(self):
+        result = '{"tasks" : [' 
+        for index in range(0, len(self.list)):
+            result += (self.list[index].toJSON())
+            if index < len(self.list) - 1:
+                result += ', '
+        result += ']}'
+        return result
 
 class TodoistClient:
     api = None
@@ -22,11 +44,15 @@ class TodoistClient:
 
 class WCFAdapter_1_0_0(TodoistClient):
     def GetTaskList(self):
+        result = TaskList()
+
         for item in self.response['items']:
             if item['checked'] == 0:
                 s = item['content']
+
                 name = ""
                 url = ""
+
                 if s.find('http') == -1:
                     name = s
                 else:
@@ -34,13 +60,15 @@ class WCFAdapter_1_0_0(TodoistClient):
                     url = l[0]
                     name = " ".join(l[1:])
 
+                #  Remove brackets.
                 if name[0] == '(':
                     name = name[1:]
                 if name[len(name) - 1] == ')':
                     name = name[:len(name) - 1]
 
-                print(name)
-                print(url)
+                result.list.append(Task_1(item['id'], name, url))
+
+        return result
 
     def FinishTask():
         pass
@@ -50,7 +78,8 @@ parser.add_argument('--configfile')
 
 def queryTodoistAPI(userName, password):
     adapter = WCFAdapter_1_0_0(userName, password)
-    adapter.GetTaskList()
+    list = adapter.GetTaskList()
+    print(list.toJSON())
 
 if __name__ == '__main__':
     args = parser.parse_args()
